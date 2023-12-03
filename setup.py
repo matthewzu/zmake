dfcdcbeb33430738b6d80939cba1fe3441ce7634
matthewzu <xiaofeng.zu@windriver.com>
2023-12-03 20:44:39 +0800

    add more detalis for package

--- setup.py
+++ setup.py
@@ -23,6 +23,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
     ],
     entry_points                    = {
         'console_scripts': [
